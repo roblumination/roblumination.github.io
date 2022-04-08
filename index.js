1855af371f172793c33b01482f1283a915857cd7
Roman Blumer <roblumination@gmail.com>
2022-04-08 18:27:09 +0300

    Add header and learn more

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 let [content] = document.querySelectorAll(".content");
 let [headerBackground] = document.querySelectorAll(".header-background");
 let [header] = document.querySelectorAll("header");
+// let [topBanner] = document.querySelectorAll(".top-banner");
+let learnMoreContainer = document.querySelector(".top-banner-scroll-down");
+let isNeedToHideHeader = true;
+
+function isScrollNearContent(pxFromBottom) {
+	if (window.scrollY > window.innerHeight - pxFromBottom) {
+		return true;
+	}
+	return false;
+}
 
 document.addEventListener("scroll", (e) => {
-	headerBackground.style.opacity = window.scrollY > 200 ? 1 : 0;
-	header.style.color =
-		window.scrollY > window.innerHeight / 2 ? "#000" : "#fff";
-	content.innerHTML = intFrameHeight;
+	headerBackground.style.opacity = isScrollNearContent(300) ? 1 : 0;
+	header.style.color = isScrollNearContent(300) ? "#000" : "#fff";
+	header.style.opacity = isNeedToHideHeader ? 0 : 1;
+	if (isScrollNearContent(300)) {
+		isNeedToHideHeader = false;
+	}
+	learnMoreContainer.style.opacity = isScrollNearContent(500) ? 0 : 1;
+	// content.innerHTML = window.innerHeight;
 });
