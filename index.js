1b6bbf97e64db1aaf68e873375476c37908d619b
Roman Blumer <roblumination@gmail.com>
2022-04-11 17:02:14 +0300

    Add footer darker line

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-let [content] = document.querySelectorAll(".content");
-let [headerBackground] = document.querySelectorAll(".header-background");
-let [header] = document.querySelectorAll("header");
-// let [topBanner] = document.querySelectorAll(".top-banner");
+let headerBackground = document.querySelector(".header-background");
+let header = document.querySelector("header");
 let learnMoreContainer = document.querySelector(".top-banner-scroll-down");
 let isNeedToHideHeader = true;
 
@@ -14,11 +12,9 @@ function isScrollNearContent(pxFromBottom) {
 
 document.addEventListener("scroll", (e) => {
 	headerBackground.style.opacity = isScrollNearContent(300) ? 1 : 0;
-	// header.style.color = isScrollNearContent(300) ? "#000" : "#fff";
 	header.style.opacity = isNeedToHideHeader ? 0 : 1;
 	if (isScrollNearContent(300)) {
 		isNeedToHideHeader = false;
 	}
 	learnMoreContainer.style.opacity = isScrollNearContent(500) ? 0 : 1;
-	// content.innerHTML = window.innerHeight;
 });
